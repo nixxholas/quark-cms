226363f9e78b0a2d89048dd82d7bb2663e179016
nixxholas <nixholas@icloud.com>
2018-11-14 23:18:28 +0800

    Delta.WebApp: Integrate Trinity Data Contexts
    Signed-off-by: nixxholas <nixholas@icloud.com>

--- Delta.WebApp/Startup.cs
+++ Delta.WebApp/Startup.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Delta.DataAccess.Contexts.Trinity;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -24,14 +26,33 @@ namespace Delta.WebApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AuthDataContext>(options =>
+            {
+                var connStr = Configuration.GetConnectionString("Trinity:Auth");
+                options.UseSqlServer(connStr);
+            });
+            services.AddDbContext<CharactersDataContext>(options =>
+            {
+                var connStr = Configuration.GetConnectionString("Trinity:Characters");
+                options.UseSqlServer(connStr);
+            });
+            services.AddDbContext<HotfixDataContext>(options =>
+            {
+                var connStr = Configuration.GetConnectionString("Trinity:Hotfixes");
+                options.UseSqlServer(connStr);
+            });
+            services.AddDbContext<WorldDataContext>(options =>
+            {
+                var connStr = Configuration.GetConnectionString("Trinity:World");
+                options.UseSqlServer(connStr);
+            });
+            
             services.Configure<CookiePolicyOptions>(options =>
             {
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
                 options.CheckConsentNeeded = context => true;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
-            
-            
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
